270ed45879ec012ecb8ccfd5ce3d3adb6042eec2
Stepan Korshakov <me@ex3ndr.com>
2014-09-29 13:22:21 +0400

    Fixed logging, updated javadocs, prepared for maven central

--- opus/src/main/java/com/droidkit/opus/OpusLib.java
+++ opus/src/main/java/com/droidkit/opus/OpusLib.java
@@ -3,30 +3,93 @@ package com.droidkit.opus;
 import java.nio.ByteBuffer;
 
 /**
- * Created by ex3ndr on 29.09.14.
+ * OpusLib native binding
  */
 public class OpusLib {
+    /**
+     * Starting opus recording
+     *
+     * @param path path to file
+     * @return non zero if started player
+     */
     public native int startRecord(String path);
 
+    /**
+     * Writing audio frame to encoder
+     *
+     * @param frame buffer with sound in 16 bit mono PCM 16000 format
+     * @param len   len of data
+     * @return not null if successful
+     */
     public native int writeFrame(ByteBuffer frame, int len);
 
+    /**
+     * Stopping record
+     */
     public native void stopRecord();
 
-    public native int openOpusFile(String path);
-
-    public native int seekOpusFile(float position);
-
+    /**
+     * Checking Opus File format
+     *
+     * @param path path to file
+     * @return non zero if opus file
+     */
     public native int isOpusFile(String path);
 
+    /**
+     * Opening file
+     *
+     * @param path path to file
+     * @return non zero if successful
+     */
+    public native int openOpusFile(String path);
+
+    /**
+     * Seeking in opus file
+     *
+     * @param position position in file
+     * @return non zero if successful
+     */
+    public native int seekOpusFile(float position);
+
+    /**
+     * Closing opus file
+     */
     public native void closeOpusFile();
 
+    /**
+     * Reading from opus file
+     *
+     * @param buffer
+     * @param capacity
+     */
     public native void readOpusFile(ByteBuffer buffer, int capacity);
 
+    /**
+     * Is playback finished
+     *
+     * @return non zero if playback is finished
+     */
     public native int getFinished();
 
+    /**
+     * Read block size in readOpusFile
+     *
+     * @return block size in bytes
+     */
     public native int getSize();
 
+    /**
+     * Offset of actual sound for playback
+     *
+     * @return offset
+     */
     public native long getPcmOffset();
 
+    /**
+     * Total opus pcm duration
+     *
+     * @return pcm duration
+     */
     public native long getTotalPcmDuration();
 }
---
+++ opus/src/main/jni/log.h
@@ -0,0 +1,11 @@
+#ifndef LOG_INCLUDED
+#define LOG_INCLUDED
+
+#include <android/log.h>
+
+#define  LOG_TAG "libopus"
+#define  LOGI(...)  __android_log_print(ANDROID_LOG_INFO,LOG_TAG,__VA_ARGS__)
+#define  LOGE(...)  __android_log_print(ANDROID_LOG_ERROR,LOG_TAG,__VA_ARGS__)
+#define  LOGD(...)  __android_log_print(ANDROID_LOG_DEBUG,LOG_TAG,__VA_ARGS__)
+
+#endif
